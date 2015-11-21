edb248624cb394b64f6343b6080671eb42f03369
Mantas Rancevas <mantas.rancevas@gmail.com>
2015-11-21 14:23:49 +0000

    Made benchmark run

--- src/com/vr/coursework/VehicleRouting.java
+++ src/com/vr/coursework/VehicleRouting.java
@@ -14,42 +14,52 @@ public class VehicleRouting
 {
 	static long currTime = System.currentTimeMillis();
 	private static File dataDir = new File("data");
-	private static File resultDir = new File("results/clarke-wright-optimized/" + currTime); 
+	private static File resultDir = new File("results/clarke-wright-benchmark/" + currTime); 
 
 	public static void main(String[] args) throws Exception
 	{
 		List<String> resultsSummary = new ArrayList<String>();
 		System.out.println("Program Started");
 		resultDir.mkdirs();// create dir for saving results
-//		resultDir.cr
-//		File dataDir = new File("data");
-//		File resultDir = new File("results");
 		
 		Files.walk(Paths.get(dataDir.getAbsolutePath())).forEach(filePath -> {
 		    if (Files.isRegularFile(filePath)) {
 		    	String name = filePath.toFile().getName();
 		    	if(name.endsWith("prob.csv"))
 		    	{
-//		    		System.out.println(name.replace(".csv", ""));
 		    		String problemName = name.replace(".csv", "");
 		    		try {
-		    			String result = solveProblem(problemName);
+//		    			String result = solveProblem(problemName);
+		    			String result = benchmarkSolutions(problemName);
 						resultsSummary.add(result);
 					} catch (Exception e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 		    	}
 		    }
 		});
-		
-
-//		String problemName = "rand00020prob";
-//		solveProblem(problemName);
+		 
 		for(String result : resultsSummary)
 			System.out.println(result);
 		
 		writeOut(resultDir.getAbsolutePath() + "/results-summary.csv", resultsSummary);
+	}
+	
+	static String benchmarkSolutions(String problemName) throws Exception
+	{
+		String result = "";
+		VRProblem p = new VRProblem(dataDir.getAbsolutePath() + "/" + problemName + ".csv");
+		String solName = problemName.replace("prob", "cwsn.csv");
+		File solFile = new File(dataDir.getAbsolutePath() + "/" + solName);
+		if(solFile.exists())
+		{
+			VRSolution s = new VRSolution(p);
+			s.readIn(solFile.getAbsolutePath());
+			double cost = s.solnCost();
+			result = problemName + "," + cost;
+		}
+		
+		return result;
 	}
 	
 	static String solveProblem(String problemName) throws Exception
